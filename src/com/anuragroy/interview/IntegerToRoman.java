2ffbbfeceef9dcae044d372eace1bd168d0c68c9
anuragroy17 <anuragroy17@gmail.com>
2019-11-21 18:51:31 +0530

    Roman Numeral Problems added

---
+++ src/com/anuragroy/interview/IntegerToRoman.java
@@ -0,0 +1,39 @@
+package com.anuragroy.interview;
+
+import java.text.ParseException;
+import java.util.Scanner;
+
+public class IntegerToRoman {
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.println("Please enter a positive value below 3999: ");
+            int num = sc.nextInt();
+            sc.nextLine();
+            if(num<3999 && num>=0){
+                System.out.println("Value in Roman Numerals: "+intToRoman(num));
+            }else{
+                System.out.println("Value not in Range");
+            }
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+
+        sc.nextLine();
+        sc.close();
+    }
+
+    private static String intToRoman(int num){
+        String[] thousands = {"", "M", "MM", "MMM"};
+        String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] units = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
+        return thousands[(num / 1000)]     //Consider 3925 => 3925/1000 = 3.925 => int = 3
+                +hundreds[(num%1000)/100]  //Consider 3925 => 3925%1000 => (3925 - 3000)Remainder is 925 => 925/100 = 9.25 => int = 9
+                +tens[(num%100)/10]        //Consider 3925 => 3925%100 => (3925 - 3900)Remainder is 25 => 25/10 = 2.5 => int = 2
+                +units[(num%10)];           //Consider 3925 => 3925%10 => (3925 - 3920)Remainder is 5
+    }
+
+}
---
+++ src/com/anuragroy/interview/RomanToInteger.java
@@ -0,0 +1,52 @@
+package com.anuragroy.interview;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class RomanToInteger {
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.println("Please a valid Roman Numeral ");
+            String roman = sc.nextLine();
+            sc.nextLine();
+            System.out.println("Value in Numerals: "+ romanToInt(roman));
+        } catch (Exception e) {
+            System.out.println("Invalid Roman Numeral");
+        }
+
+        sc.nextLine();
+        sc.close();
+    }
+
+    private static int romanToInt(String roman) {
+        Map<Character, Integer> mapping = new HashMap<>();
+        mapping.put('I',1);
+        mapping.put('V',5);
+        mapping.put('X',10);
+        mapping.put('L',50);
+        mapping.put('C',100);
+        mapping.put('D',500);
+        mapping.put('M',1000);
+        mapping.put('i',1);
+        mapping.put('v',5);
+        mapping.put('x',10);
+        mapping.put('l',50);
+        mapping.put('c',100);
+        mapping.put('d',500);
+        mapping.put('m',1000);
+
+        int result = 0;
+
+        for (int i=0; i<roman.length(); i++) {
+            if (i > 0 && mapping.get(roman.charAt(i))>mapping.get(roman.charAt(i-1)))
+                result += mapping.get(roman.charAt(i)) - 2 * mapping.get(roman.charAt(i-1));
+            else
+            result += mapping.get(roman.charAt(i));
+        }
+
+        return result;
+    }
+}
